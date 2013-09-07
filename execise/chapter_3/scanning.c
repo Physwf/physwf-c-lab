f355ad2ccb03168a69fdbd486d8e50301adaaa09
Physwf <joe.physwf@hotmail.com>
2013-09-07 02:10:11 -0700

    modify scanning.c

--- execise/chapter_3/scanning.c
+++ execise/chapter_3/scanning.c
@@ -2,65 +2,33 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <dirent.h>
-#include <stack>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <string.h>
+
+void list_dir(char *path)
+{
+	DIR *pDir;
+	struct dirent *pEnt;
+	char childpath[512];
+	pDir = opendir(path);
+	
+	memset(childpath,0,sizeof(childpath));
+	while((pEnt = readdir(pDir)) != NULL)
+	{
+		if(strcmp(pEnt->d_name,".") == 0 || strcmp(pEnt->d_name,"..") == 0) continue;
+		if(pEnt->d_type & DT_DIR)
+		{
+			sprintf(childpath,"%s/%s",path,pEnt->d_name);
+			printf("%s\n",childpath);
+			list_dir(childpath);
+		}
+	}
+}
 
 int main(int argc, char **argv)
 {
-	char *root_path;
-	DIR *root;
-	
-	std::stack<DIR*> dir_stack;
-	
-	if(argc <= 1)
-	{
-		printf("no directory supplied!\n");
-		exit(1);
-	}
-	root_path = argv[1];
-	//struct stat *root_stat;
-	//stat(root_path,root_stat);
-	//if(S_ISDIR(root_stat->st_mode))
-	//{
-	//	printf("root is dir\n");
-	//}
-	
-	root = opendir(root_path);
-	if(root == NULL)
-	{
-		printf("Root directory error!\n");
-		exit(1);
-	}
-	chdir(root_path);
-
-	dir_stack.push(root);
-	DIR *cur_dir;
-	struct dirent *entry = NULL;
-	while(!dir_stack.empty())
-	{
-		cur_dir = dir_stack.top();
-		dir_stack.pop();
-		
-		struct stat *fstat;
-		while((entry = readdir(cur_dir))!=NULL)
-		{
-			stat(entry->d_name,fstat);
-			//if(S_ISDIR(fstat->st_mode))
-			//{
-			//	DIR *dir = opendir(entry->d_name);
-			//	printf("find dir:%s\n",entry->d_name);
-				//dir_stack.push(dir);
-			//	printf("dir\n");
-			//	continue;
-			//}
-			if(S_ISREG(fstat->st_mode))
-			{
-				printf("the file name is:%s \nthe mtime is:%d\n",entry->d_name,fstat->st_mtime);
-			}
-		}	
-	}
-	
-	exit(0);	
+	list_dir(argv[1]);	
+	return 0;
 }
