3ea82ffb83228aa3486310d9f60c2e51e3815d37
physwf <joe.physwf@hotmail.com>
2014-01-12 00:38:10 +0800

    modify buffer_object_test add triangles files

--- OpenGL/RedBook/chapter_1/triangles.cpp
+++ OpenGL/RedBook/chapter_1/triangles.cpp
@@ -2,8 +2,104 @@
 #include <GL/glew.h>
 #include <GL/glut.h>
 #include "vgl.h"
+#include "LoadShaders.h"
+
+enum VAO_IDs { Triangles,NumVAOs};
+enum Buffer_IDs { ArrayBuffer,NumBuffers };
+enum Attrib_IDs { vPosition=0 };
+
+GLuint VAOs[NumVAOs];
+GLuint Buffers[NumBuffers];
+
+const GLuint NumVertices =6;
 
 int main(int argc,char *args[])
 {
 	
 }
+
+void init()
+{
+	glGenVertexArray(NumVAOs,VAOs);
+	glBindVertexArray(VAOs[Triangles]);
+	
+	GLfloat vertices[NumVertices][2] = {
+		{-0.90,-0.90},
+		{0.85,-0.90},
+		{-0.90,0.85},
+		{0.90,0.85},
+		{0.90,0.90},
+		{-0.85,0.90}
+	};
+	
+	glGenBuffers(NumBuffers,Buffers);
+	glBindBuffer(GL_ARRAY_BUFFER,Buffers[ArrayBuffer]);
+	glBufferData(GL_ARRAY_BIFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
+	
+	ShaderInfo shaders[] = {
+		{GL_VERTEX_SHADER,"triangles.vert"},
+		{GL_FRAGMENT_SHADER,"triangles.frag"},
+		{GL_NONE,NULL}
+	};
+	
+	GLuint program = LoadShaders(shaders);
+	glUseProgram(program);
+	
+	glVertexAttribPointer(vPosition,2,GL_FLOAT,GL_FALSE,0,BUFFER_OFFSET(0));
+	glEnableVertexAttribArray(vPosition);
+}
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	
+	glBindVertexArray(VAOs[Triangles]);
+	glDrawArrays(GL_TRIANGLES,0,NumVertices);
+	
+	glFlush();
+}
+
+int main(int argc,char** argv)
+{
+	glutInit(&argc,argv);
+	gluiInitDisplayMode(GLUT_RGBA);
+	glutInitWindowSize(512,512);
+	glutInitWindowPosition(0,0);
+	glutInitCreateProfile(GLUT_CORE_PROFILE);
+	glutCreateWindow("Triangles");
+	GLenum err = glewInit();
+	if(err!=GLEW_OK)
+	{
+		printf("Glew Init Error!");
+		return 1;
+	}
+	init();
+	glutDisplayFunc(&display);
+	glutMainLoop();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- OpenGL/RedBook7/chapter_2/buffer_object_test.cpp
+++ OpenGL/RedBook7/chapter_2/buffer_object_test.cpp
@@ -5,22 +5,6 @@
 #define INDICES 1
 #define NUM_BUFFERS 2
 
-GLuint buffers[NUM_BUFFERS];
-
-GLfloat vertices[][2] =
-{
-	{100.0,100.0},
-	{100.0,200.0},
-	{200.0,200.0},
-	{200.0,100.0}
-};
-
-GLubyte indices[][4] =
-{
-	{0,1,2,3}
-};
-
-
 void draw()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
@@ -36,6 +20,60 @@ void onResize(int w,int h)
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluOrtho2D(0.0,(GLdouble)w,0.0,(GLdouble)h);
+}
+
+void initBuffers()
+{
+	GLuint buffers[NUM_BUFFERS];
+
+	GLfloat vertices[] =
+	{
+		100.0,100.0,
+		100.0,200.0,
+		200.0,200.0,
+		200.0,100.0,
+	};
+
+	GLubyte indices[] =
+	{
+		0,1,2,3
+	};
+	glGenBuffers(NUM_BUFFERS,buffers);
+	glBindBuffer(GL_ARRAY_BUFFER,buffers[VERTICES]);
+	glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
+	glVertexPointer(2,GL_FLOAT,0,BUFFER_OFFSET(0));
+	glEnableClientState(GL_VERTEX_ARRAY);
+	
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,buffers[INDICES]);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
+}
+
+void initBuffersUseStride()
+{
+	GLuint buffers[NUM_BUFFERS];
+
+	GLfloat vertices[] =
+	{
+		100.0,100.0,1.0,0.0,0.0,
+		100.0,200.0,1.0,1.0,0.0,
+		200.0,200.0,1.0,0.0,1.0,
+		200.0,100.0,0.0,1.0,0.0
+	};
+
+	GLubyte indices[] =
+	{
+		0,1,2,3
+	};
+	glGenBuffers(NUM_BUFFERS,buffers);
+	glBindBuffer(GL_ARRAY_BUFFER,buffers[VERTICES]);
+	glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
+	glVertexPointer(2,GL_FLOAT,5*sizeof(GLfloat),BUFFER_OFFSET(0));
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glColorPointer(3,GL_FLOAT,5*sizeof(GLfloat),BUFFER_OFFSET(2*sizeof(GLfloat)));
+	glEnableClientState(GL_COLOR_ARRAY);
+	
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,buffers[INDICES]);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
 }
 
 int main(int argc,char *argv[])
@@ -55,14 +93,8 @@ int main(int argc,char *argv[])
 	}
 	//set clear color
 	glClearColor(0.0,0.0,0.0,1.0);
-	glGenBuffers(NUM_BUFFERS,buffers);
-	glBindBuffer(GL_ARRAY_BUFFER,buffers[VERTICES]);
-	glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
-	glVertexPointer(2,GL_FLOAT,0,BUFFER_OFFSET(0));
-	glEnableClientState(GL_VERTEX_ARRAY);
-	
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,buffers[INDICES]);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
+	// initBuffers();
+	initBuffersUseStride();
 	glutReshapeFunc(&onResize);
 	glutDisplayFunc(&draw);
 	glutMainLoop();
