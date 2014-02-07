6dd68fff3eb720c474cebcbd7ba58b888b875c42
unknown <YueleiQiao@PSHAENGDESK027.aaa.ad.ea.com>
2014-02-07 17:44:34 +0800

    add DLL-lab

---
+++ Dll-Lab/HelloWorld/VC/VC/VC.cpp
@@ -0,0 +1,22 @@
+// VC.cpp : 定义 DLL 应用程序的导出函数。
+//
+
+#include "stdafx.h"
+#include "VC.h"
+
+
+// 这是导出变量的一个示例
+VC_API int nVC=0;
+
+// 这是导出函数的一个示例。
+VC_API int fnVC(void)
+{
+	return 42;
+}
+
+// 这是已导出类的构造函数。
+// 有关类定义的信息，请参阅 VC.h
+CVC::CVC()
+{
+	return;
+}
