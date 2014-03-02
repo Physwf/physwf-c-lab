d17204809b960e0dc16f7de6988358ae2e0996a2
physwf <joe.physwf@hotmail.com>
2014-03-02 21:50:45 +0800

    fix taskbar icon problem

--- Win32/CreateWindow/main.cpp
+++ Win32/CreateWindow/main.cpp
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <tchar.h>
+#include <stdio.h>
 #include <GL/GL.h>
+#include <time.h>
 
 #pragma comment (lib,"kernel32.lib")
 #pragma comment (lib,"user32.lib")
@@ -11,8 +13,15 @@
 
 LRESULT CALLBACK WndProc(HWND hWnd,UINT msg,WPARAM wPara,LPARAM lPara);
 
+time_t appTime;
+float FPS;
+int interval;
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+	FPS = 1/60;
+	interval = 1000*FPS;
+	HWND hwnd;
 	MSG msg 		= {0};
 	WNDCLASSEX wc	= {0};
 	wc.cbSize 		= sizeof(WNDCLASSEX);
@@ -21,14 +30,27 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	wc.hbrBackground = (HBRUSH)(COLOR_BACKGROUND);
 	wc.lpszClassName = "Hello,world";
 	wc.style = CS_OWNDC;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
 	wc.hCursor		= LoadCursor(NULL,IDC_ARROW);
-	wc.hIcon		= LoadIconW(wc.hInstance,MAKEINTRESOURCEW(IDI_APPLICATION));
+	wc.hIcon		= LoadIconW(NULL,MAKEINTRESOURCEW(IDI_APPLICATION));
 	wc.hIconSm		= LoadIconW(wc.hInstance,MAKEINTRESOURCEW(IDI_APPLICATION));
 	if(!RegisterClassEx(&wc))
 		return 1;
-	CreateWindowW(L"Hello,World",L"Hello,World",WS_OVERLAPPEDWINDOW|WS_VISIBLE,0,0,640,480,0,0,hInstance,0);
-	while(GetMessage(&msg,NULL,0,0) > 0)
-		DispatchMessage(&msg);
+	hwnd = CreateWindowW(L"Hello,World",L"Hello,World",WS_OVERLAPPEDWINDOW|WS_VISIBLE,0,0,640,480,0,0,hInstance,0);
+	// ShowWindow(hwnd, nCmdShow);              //display the window on the screen
+	// UpdateWindow(hwnd);
+	while(1)
+	{
+		if(GetMessage(&msg,NULL,0,0) > 0)
+			DispatchMessage(&msg);
+		time_t elapse = time(NULL) - appTime;
+		if(elapse >= interval)
+		{
+			appTime += elapse;
+		}
+	}
+		
 	
 	return 0;
 }
@@ -66,8 +88,9 @@ LRESULT CALLBACK WndProc(HWND hWnd,UINT msg,WPARAM wPara,LPARAM lPara)
 				HGLRC ourOpenGLRenderingContext = wglCreateContext(ourWindowHandleToDeviceContext);
 				wglMakeCurrent(ourWindowHandleToDeviceContext,ourOpenGLRenderingContext);
 				
-				MessageBoxA(0,(char*)glGetString(GL_VERSION),"OPENGL VERSION",0);
+				printf("opengl version: %s\n",glGetString(GL_VERSION));
 				
+				MessageBoxA(0,(char*)glGetString(GL_VERSION),"OPENGL VERSION",0);
 			}
 			break;
 		case WM_DESTROY:
