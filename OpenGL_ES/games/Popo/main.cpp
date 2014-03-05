1d9c1fb84e1900f50b65151b328df68b3ce6a767
unknown <YueleiQiao@PSHAENGDESK027.aaa.ad.ea.com>
2014-03-05 17:45:00 +0800

    add Popo

---
+++ OpenGL_ES/games/Popo/main.cpp
@@ -0,0 +1,153 @@
+#include <EGL/egl.h>
+#include <stdio.h>
+#include <windows.h>
+
+#pragma comment (lib,"kernel32.lib")
+#pragma comment (lib,"user32.lib")
+#pragma comment (lib,"Gdi32.lib")
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch(msg)
+	{
+		case WM_CLOSE:
+			PostQuitMessage(0);
+			break;
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			break;
+		default:
+			// return DefWindowProc(hWnd,msg,wParam,lParam);
+			break;
+	}
+	return DefWindowProc(hWnd,msg,wParam,lParam);
+}
+
+HWND create_window(int w,int h)
+{
+	HWND window;
+	WNDCLASS wc;
+	RECT rect;
+	HINSTANCE hInstance;
+	
+	rect.left = 0L;
+	rect.right = (long)w;
+	rect.top = 0L;
+	rect.bottom = (long)h;
+	
+	hInstance = GetModuleHandle(NULL);
+	
+	wc.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(NULL,IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL,IDC_ARROW);
+	wc.hbrBackground = NULL;
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = "Popo";
+	
+	RegisterClass(&wc);
+	
+	AdjustWindowRectEx(&rect,WS_OVERLAPPEDWINDOW,FALSE,WS_EX_APPWINDOW|WS_EX_WINDOWEDGE);
+	
+	window = CreateWindowEx(
+	WS_EX_APPWINDOW|WS_EX_WINDOWEDGE,
+	"Popo",
+	"Popo",
+	WS_OVERLAPPEDWINDOW|WS_CLIPSIBLINGS|WS_CLIPCHILDREN,
+	0,0,
+	rect.right - rect.left,
+	rect.bottom - rect.top,
+	NULL,NULL,
+	hInstance,
+	NULL );
+	
+	ShowWindow(window,SW_SHOW);
+	SetForegroundWindow(window);
+	SetFocus(window);
+	
+	return window;
+}
+
+int main()
+{
+	EGLint majorVersion;
+	EGLint minorVersion;
+	EGLDisplay display;
+	
+	display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+	if(display == EGL_NO_DISPLAY)
+	{
+		printf("Unable to open connection to local windowing system!\n");
+	}
+	if(!eglInitialize(display,&majorVersion,&minorVersion))
+	{
+		printf("Unable to initialize EGL!");
+	}
+	
+	EGLint attribList[] = 
+	{
+		EGL_RENDERABLE_TYPE,EGL_OPENGL_ES2_BIT,
+		EGL_RED_SIZE,8,
+		EGL_GREEN_SIZE,8,
+		EGL_BLUE_SIZE,8,
+		EGL_DEPTH_SIZE,16,
+		EGL_NONE
+	};
+	
+	const EGLint MaxConfigs = 10;
+	EGLConfig configs[MaxConfigs];
+	EGLint numConfigs;
+	
+	if(!eglChooseConfig(display,attribList,configs,MaxConfigs,&numConfigs))
+	{
+		printf("Choose Config failed!\n");
+	}
+	
+	HWND window = create_window(500,400);
+	
+	EGLSurface surface;
+	surface = eglCreateWindowSurface(display,configs[0],window,NULL);
+	
+	if(surface == EGL_NO_SURFACE)
+	{
+		printf("Surface create error!\n");
+	}
+	
+	EGLContext context;
+	EGLint contextAttribList[] = 
+	{
+		EGL_CONTEXT_CLIENT_VERSION,2,
+		EGL_NONE
+	};
+	context = eglCreateContext(display,configs[0],EGL_NO_CONTEXT,contextAttribList);
+	
+	if(context == EGL_NO_CONTEXT)
+	{
+		printf("Create context error!\n");
+	}
+	
+	eglMakeCurrent(display,window,window,context);
+	
+	bool done = false;
+	MSG msg;
+	while(!done)
+	{
+		if(PeekMessage(&msg,NULL,0,0,PM_REMOVE))
+		{
+			if(msg.message == WM_QUIT)
+			{
+				done = true;
+			}
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else
+		{
+			
+		}
+	}
+	return 0;
+}
