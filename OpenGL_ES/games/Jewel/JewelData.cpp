cb20227c57cd2db0521fa6fd22ef2bc9ed965b04
physwf <joe.physwf@hotmail.com>
2014-04-14 17:36:11 +0800

    Jewel data

--- OpenGL_ES/games/Jewel/JewelData.cpp
+++ OpenGL_ES/games/Jewel/JewelData.cpp
@@ -5,5 +5,21 @@ int num_jewels;
 
 void initData()
 {
+	int num_columns = 8;
+	int num_rows = 8;
+	num_jewels = num_columns * num_rows;
+	jewels = (int*)malloc(num_jewels*sizeof(int));
+	memset(jewels,0,num_jewels*sizeof(int));
 	
+	for(int i=0;i<num_jewels;i++)
+	{
+		int* jewel = jewels+i;
+		*jewel = random_by_range(JEWEL_TYPE_1,JEWEL_TYPE_MAX);
+	}
+}
+
+void checkElimination()
+{
+	int checked_list[100];
+	int num_checked = 0;
 }
--- OpenGL_ES/games/Jewel/JewelData.h
+++ OpenGL_ES/games/Jewel/JewelData.h
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+#define JEWEL_TYPE_1 1
+#define JEWEL_TYPE_2 2
+#define JEWEL_TYPE_3 3
+#define JEWEL_TYPE_4 4
+#define JEWEL_TYPE_MAX 5
 extern int* jewels;
 extern int num_jewels;
 
