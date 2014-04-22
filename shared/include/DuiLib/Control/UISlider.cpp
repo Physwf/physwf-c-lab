b750aec61fafd18b03f40effab5df930e60dc265
physwf <joe.physwf@hotmail.com>
2014-04-22 16:20:53 +0800

    modify CSiliderUI valuechanged event

--- shared/include/DuiLib/Control/UISlider.cpp
+++ shared/include/DuiLib/Control/UISlider.cpp
@@ -162,11 +162,13 @@ namespace DuiLib
 					if( event.ptMouse.x >= m_rcItem.right - m_szThumb.cx / 2 ) m_nValue = m_nMax;
 					else if( event.ptMouse.x <= m_rcItem.left + m_szThumb.cx / 2 ) m_nValue = m_nMin;
 					else m_nValue = m_nMin + (m_nMax - m_nMin) * (event.ptMouse.x - m_rcItem.left - m_szThumb.cx / 2 ) / (m_rcItem.right - m_rcItem.left - m_szThumb.cx);
+					m_pManager->SendNotify(this, DUI_MSGTYPE_VALUECHANGED);
 				}
 				else {
 					if( event.ptMouse.y >= m_rcItem.bottom - m_szThumb.cy / 2 ) m_nValue = m_nMin;
 					else if( event.ptMouse.y <= m_rcItem.top + m_szThumb.cy / 2  ) m_nValue = m_nMax;
 					else m_nValue = m_nMin + (m_nMax - m_nMin) * (m_rcItem.bottom - event.ptMouse.y - m_szThumb.cy / 2 ) / (m_rcItem.bottom - m_rcItem.top - m_szThumb.cy);
+					m_pManager->SendNotify(this, DUI_MSGTYPE_VALUECHANGED);
 				}
 				Invalidate();
 			}
