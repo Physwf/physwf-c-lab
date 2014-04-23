088a7a3f863c0c1e56e3b838f705477369e2b35a
physwf <joe.physwf@hotmail.com>
2014-04-23 18:18:15 +0800

    jewel in develop

--- OpenGL_ES/games/Jewel/JewelData.cpp
+++ OpenGL_ES/games/Jewel/JewelData.cpp
@@ -1,4 +1,17 @@
 #include "JewelData.h"
+#include <math.h>
+
+#define NUM_ROWS 8
+#define NUM_COLS 8
+
+typedef struct elimination_area_t {
+	int type;
+	int orgin;
+	int num_heriz;
+	int num_verti;
+	int indices[100];
+	int num_indices;
+} elim_area, *elim_area_p;
 
 int* jewels;
 int num_jewels;
@@ -20,6 +33,94 @@ void initData()
 
 void checkElimination()
 {
-	int checked_list[100];
-	int num_checked = 0;
+	bool in_horiz_flag[100];
+	bool in_verti_flag[100];
+	elim_area areas[30];
+	memset(areas,0,sizeof(areas));
+	int num_areas = 0;
+	for(int i=0;i<num_jewels;i++)
+	{
+		int row = i / 8;
+		int col = i % 8;
+		elim_area area = areas[num_areas];
+		area.type = *(jewel+i);
+		area.orgin = i;
+		area.num_horiz = 1;
+		area.num_verti = 1;
+		//right
+		int right = i;
+		while(right++ / 8 == row)
+		{
+			if(!in_horiz_flag[right])
+			{
+				if( *(jewel+right) == *(jewel+i)) continue;
+				else break;
+			}
+		}
+		if( right - i) > 2)
+		{
+			for(int j=i;j<right;j++)
+			{
+				in_horiz_flag[j] = true;
+				area.num_horiz ++;
+				area.indices[area.num_indices] = j;
+				area.num_indices ++;
+			}
+		}
+		
+		int down = i;
+		down+=NUM_COLS;
+		while( down % 8 == col)
+		{
+			if(!in_verti_flag[down])
+			{
+				if( *(jewel+down) == *(jewel+i)) continue;
+				else break;
+			}
+		}
+		if(( down - i) / 8 > 2)
+		{
+			for(int j=i;j<down;j+=8)
+			{
+				bool in_verti_flag[j] = true;
+				area.num_verti ++;
+				area.indices[area.num_indices] = j;
+				area.num_indices ++;
+			}
+		}
+		
+		if(area.num_heriz > 2 || area.num_verti > 2) num_areas++;
+	}
+	
+	for(int i=0;i<num_areas;i++)
+	{
+		elim_area area = areas[i];
+		for(int j=0;j<area.num_indices;j++)
+		{
+			int type = *(jewels+area.indices[j]);
+			//if(type == ) check bomb type
+		}
+
+		if(area.num_heriz > 4 || area.num_verti > 4)//diamen
+		{
+			*(jewels+area.orgin) = JEWEL_TYPE_DIAMOND;
+		}
+		else if(area.num_heriz > 3)
+		{
+			*(jewels+area.orgin) = BOMB_TYPE_V_BASE+1;
+		}
+		else if(area.num_verti > 3)
+		{
+			*(jewels+area.orgin) = BOMB_TYPE_H_BASE+1;
+		}
+		else if(area.num_heriz > 2 && area.num_verti > 2)
+		{
+			
+		}
+		else
+		{
+		}
+	}
 }
+
+
--- OpenGL_ES/games/Jewel/JewelData.h
+++ OpenGL_ES/games/Jewel/JewelData.h
@@ -4,7 +4,35 @@
 #define JEWEL_TYPE_2 2
 #define JEWEL_TYPE_3 3
 #define JEWEL_TYPE_4 4
-#define JEWEL_TYPE_MAX 5
+#define JEWEL_TYPE_5 5
+#define JEWEL_TYPE_6 6
+
+#define BOMB_TYPE_V_BASE 6
+#define BOMB_TYPE_1_H 7
+#define BOMB_TYPE_2_H 8
+#define BOMB_TYPE_3_H 9
+#define BOMB_TYPE_4_H 10
+#define BOMB_TYPE_5_H 11
+#define BOMB_TYPE_6_H 12
+
+#define BOMB_TYPE_H_BASE 12
+#define BOMB_TYPE_1_V 13
+#define BOMB_TYPE_2_V 14
+#define BOMB_TYPE_3_V 15
+#define BOMB_TYPE_4_V 16
+#define BOMB_TYPE_5_V 17
+#define BOMB_TYPE_6_V 18
+
+#define BOMB_TYPE_BASE 18
+#define BOMB_TYPE_1_V 19
+#define BOMB_TYPE_2_V 20
+#define BOMB_TYPE_3_V 21
+#define BOMB_TYPE_4_V 22
+#define BOMB_TYPE_5_V 23
+#define BOMB_TYPE_6_V 24
+
+#define JEWEL_TYPE_DIAMOND 25
+
 extern int* jewels;
 extern int num_jewels;
 
