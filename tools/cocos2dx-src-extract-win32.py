206f5f4e8387fbfb5f42ead66542efede4b055e9
unknown <YueleiQiao@PSHAENGDESK027.aaa.ad.ea.com>
2014-07-10 18:21:45 +0800

    add tools

---
+++ tools/cocos2dx-src-extract-win32.py
@@ -0,0 +1,14 @@
+import os,sys
+import string
+
+print 'automake: generate make file'
+
+drive=''
+
+if sys.argc == 0
+	drive = 'D:/'
+else
+	drive = sys.argv[0];
+	if drive in 
+sourceroot = 'cocos2d-x-2.2.4/'
+targetroot = 'physwf-c-lab/'
