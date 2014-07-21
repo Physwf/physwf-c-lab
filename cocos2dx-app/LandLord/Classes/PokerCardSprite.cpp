e8567d5f0ec6fe96501aaffbf3253d005f7ef097
physwf <joe.physwf@hotmail.com>
2014-07-21 17:49:19 +0800

    add class PokerCardSprite

---
+++ cocos2dx-app/LandLord/Classes/PokerCardSprite.cpp
@@ -0,0 +1,11 @@
+#include "PokerCardSprite.h"
+
+PokerCardSprite::PokerCardSprite(int rank, Suit suit)
+{
+
+}
+
+PokerCardSprite* PokerCardSprite::create(int rank, Suit suit)
+{
+
+}
---
+++ cocos2dx-app/LandLord/Classes/PokerCardSprite.h
@@ -0,0 +1,18 @@
+#include "cocos2d.h"
+#include "../shared/logic.h"
+
+class PokerCardSprite :public cocos2d::CCSprite
+{
+public:
+	PokerCardSprite(int rank, Suit suit);
+	~PokerCardSprite();
+
+	static PokerCardSprite* create(int rank, Suit suit);
+
+private:
+	cocos2d::CCSprite mFrontBg;
+	cocos2d::CCSprite mBackBg;
+	cocos2d::CCSprite mRankSprite;
+	cocos2d::CCSprite mSmallSuitSprite;
+	cocos2d::CCSprite mBigSuitSprite;
+};
