3cebb2dd3865e5d4db6e4b2c3f4106da20781d33
physwf <joe.physwf@hotmail.com>
2014-08-03 23:06:00 +0800

    LLSceneController

--- cocos2dx-app/LandLord/Classes/LLSceneController.cpp
+++ cocos2dx-app/LandLord/Classes/LLSceneController.cpp
@@ -1,5 +1,9 @@
 #include "LLSceneController.h"
 #include "game.h"
+#include "cocos2d.h"
+#include "cocos-ext.h"
+using namespace cocos2d;
+using namespace cocos2d::extension;
 
 LLSceneController::LLSceneController()
 {
@@ -13,7 +17,15 @@ LLSceneController::~LLSceneController()
 
 void LLSceneController::showWaitForMorePlayers()
 {
+	CCTexture2D *tStart = new CCTexture2D();
+	CCSize size(92,36);
+	tStart->initWithString("Start", "Arial", 15, size, kCCTextAlignmentCenter, kCCVerticalTextAlignmentCenter);
+	
+	CCSpriteFrame * fButton = new CCSpriteFrame();
+	CCRect rect(0, 0, size.width,size.height);
+	fButton->initWithTexture(tStart, rect);
 
+	CCScale9Sprite
 };
 
 void LLSceneController::showPlayerJoin()
@@ -23,7 +35,7 @@ void LLSceneController::showPlayerJoin()
 
 void LLSceneController::showDealResult()
 {
-
+	
 };
 
 void LLSceneController::showLootScore()
